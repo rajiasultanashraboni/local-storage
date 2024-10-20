cd8aca5616ec2b096d793779487d837adce6c5ec
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-10-20 12:23:57 +0600

    array methods of map is practices here

---
+++ array-methods.js
@@ -0,0 +1,16 @@
+const products = [
+  { name: "laptop", price: 3200, brand: "hp", color: "black" },
+  { name: "camera", price: 1200, brand: "canon", color: "red" },
+  { name: "watch", price: 2200, brand: "casio", color: "green" },
+  { name: "phone", price: 3500, brand: "samsung", color: "yellow" },
+  { name: "sunglass", price: 2000, brand: "rebon", color: "white" },
+];
+
+const brands = products.map((product) => product.brand);
+const prices = products.map((product) => product.price);
+const names = products.map((product) => product.name);
+const colors = products.map((product) => product.color);
+console.log(brands);
+console.log(prices);
+console.log(names);
+console.log(colors);
