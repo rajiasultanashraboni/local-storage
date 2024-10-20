5fa431cd845ee0939039a39f6c3718ff1e286979
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-10-20 12:39:44 +0600

    forEach function is [ractices here

--- array-methods.js
+++ array-methods.js
@@ -5,12 +5,20 @@ const products = [
   { name: "phone", price: 3500, brand: "samsung", color: "yellow" },
   { name: "sunglass", price: 2000, brand: "rebon", color: "white" },
 ];
-
+// map
 const brands = products.map((product) => product.brand);
 const prices = products.map((product) => product.price);
 const names = products.map((product) => product.name);
 const colors = products.map((product) => product.color);
-console.log(brands);
-console.log(prices);
-console.log(names);
-console.log(colors);
+// console.log(brands);
+// console.log(prices);
+// console.log(names);
+// console.log(colors);
+
+// forEach
+
+products.forEach((product) => console.log(product));
+products.forEach((product) => console.log(product.color));
+products.forEach((product) => console.log(product.name));
+products.forEach((product) => console.log(product.brand));
+products.forEach((product) => console.log(product.price));
