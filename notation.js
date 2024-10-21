909258eed5fbcb57e0835ecea5476e319c651baa
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-10-21 12:55:22 +0600

    notation is practices here

---
+++ notation.js
@@ -0,0 +1,18 @@
+const student = {
+  name: "lal mia",
+  age: 35,
+  profession: "doctor",
+  25: "summer",
+  "jan-man": "risk",
+};
+
+const prof1 = student.age;
+// console.log(prof1);
+const sName = student["name"];
+// console.log(sName);
+
+const sum = student[25];
+// console.log(sum);
+
+const jan = student["jan-man"];
+console.log(jan);
